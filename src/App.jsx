09c11d60586dd6198d89d9b91bceaa75f17acf7b
jimlatantony <jimlatantony222@gmail.com>
2024-09-23 12:29:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Example from '../pages/HomePage';  
+function App() {
+
+
+  return (
+    <>
+       <div className="App">
+      
+      <Example />
+    </div>
+    </>
+  )
+}
+
+export default App
